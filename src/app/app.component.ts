c28b7833ffc3d4f1dae50d11264305d66e356c2d
mouhamed <coulibalymouhamed27@gmail.com>
2023-03-01 19:56:40 +0000

    Start add image carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [NgbCarouselConfig] 
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TCHENGA';
+
+  images = ['../../assets/img/carousel/don1.jpg',
+  '../../assets/img/carousel/don2.jpg',
+  '../../assets/img/carousel/don3.jpg'
+];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  
 }
