dde964aaeb02b84f34053d9e4dc1daf1db424ba3
mouhamed <coulibalymouhamed27@gmail.com>
2023-03-04 08:34:45 +0000

    structuring the project and adding the java project

---
+++ Java Spring Boot final project/TCHENGA/src/main/java/com/ada/ci/project/TchengaApplication.java
@@ -0,0 +1,13 @@
+package com.ada.ci.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TchengaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TchengaApplication.class, args);
+	}
+
+}
